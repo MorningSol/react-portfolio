fee148ac903cabeea4ebdd884027f34b7fd1ed79
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-07-02 13:13:40 -0400

    setup component file structure

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+
+
+function App() {
+  return (
+    <div>
+    <Header></Header>  
+    <main>
+      <About></About>
+    </main>
+    <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/About/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className="my-5">
+            <h2 id="about">About Me</h2>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui nisi enim aspernatur inventore rerum magnam officiis reiciendis, atque molestiae unde quam, excepturi voluptatum sapiente suscipit reprehenderit modi mollitia nemo dicta!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur earum voluptatibus quam aliquam ad esse autem magni eum corrupti, repellendus fuga debitis reprehenderit sunt aperiam quae vitae ut inventore veniam!
+            </p>
+        </section>
+    );
+}
+  
+  export default About;
