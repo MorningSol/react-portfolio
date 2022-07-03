e66064f6075b61caf02bea49059386f041667689
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-07-03 12:47:21 -0400

    added about me component

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import Avatar from '../../assets/img/avatar.png'
 
 function About() {
     return (
-        <section className="my-5">
-            <h2 id="about">About Me</h2>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui nisi enim aspernatur inventore rerum magnam officiis reiciendis, atque molestiae unde quam, excepturi voluptatum sapiente suscipit reprehenderit modi mollitia nemo dicta!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur earum voluptatibus quam aliquam ad esse autem magni eum corrupti, repellendus fuga debitis reprehenderit sunt aperiam quae vitae ut inventore veniam!
-            </p>
+        <section className="card m-5 p-3">
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src={Avatar} className="img-fluid rounded" alt="..."/> 
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body pb-0">
+                        <h5 className="card-title">About Me</h5> 
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui nisi enim aspernatur inventore rerum magnam officiis reiciendis, atque molestiae unde quam, excepturi voluptatum sapiente suscipit reprehenderit modi mollitia nemo dicta!
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur earum voluptatibus quam aliquam ad esse autem magni eum corrupti, repellendus fuga debitis reprehenderit sunt aperiam quae vitae ut inventore veniam!
+                        </p>  
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
   
   export default About;
+
