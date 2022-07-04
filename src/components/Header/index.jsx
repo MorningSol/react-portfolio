f6250a2f445f2d9347c30ab14b2dca9d13308eee
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-07-04 17:09:36 -0400

    added basic navbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Nav from '../Navigation';
 
 function Header() {
     return (
-        <header className='py-5'>
+        <header className='d-flex flex-row justify-content-between pt-5'>
             <h1>Patrick Laplante</h1>
+            <Nav></Nav>
         </header>
     );
 }
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,1 +1,30 @@
 import React from 'react';
+
+
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-expand-lg fs-5">
+            <div className="container-fluid">
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                        <a className="nav-link"  href="#about">About Me</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#projects">Projects</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#contact">Contact</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#resume">Resume</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
