46728e5c20b8f3bad0e1648ce211166170897585
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-08-18 14:28:29 -0400

    added email link and updated resume d/l

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,10 +8,15 @@ function Footer() {
     return (
         <footer className='mt-auto pt-4 pb-3'>
             <div className='container'>
-                <div className='row justify-content-center'>
+                <div className='row justify-content-center text-center'>
                     <div className='m-3 p-0 col-2 col-md-1'>
                         <a className='link-dark' rel='noreferrer' href="https://github.com/MorningSol" target="_blank">
                             <i className="fa-brands fa-github-square fa-4x"></i>
+                        </a>
+                    </div>
+                    <div className='m-3 p-0 col-2 col-md-1'>
+                        <a className='link-dark' rel='noreferrer'  href="mailto:patrick.laplante21@gmail.com" target="_blank">
+                            <i className="fa-solid fa-envelope fa-4x"></i>
                         </a>
                     </div>
                     <div className='m-3 p-0 col-2 col-md-1'>
@@ -19,6 +24,7 @@ function Footer() {
                             <i className="fa-brands fa-linkedin fa-4x"></i>
                         </a>
                     </div>
+                   
                 </div>
             </div>
         </footer>
